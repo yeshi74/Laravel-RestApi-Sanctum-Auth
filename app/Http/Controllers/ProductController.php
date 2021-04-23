bdf7e2f46d1d7348e5c25ea336ef354e33a6387e
yeshi <Yeshi@DESKTOP-JJA5H9E>
2021-04-23 13:52:57 +0530

    Product list, add, show Api

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+
+    //api to show single product
+    public function show($id)
+    {
+        $product = Product::where('id', $id)
+        ->select('name', 'price', 'description')
+        ->findOrFail($id);
+        $out['status'] = 'success';
+        $out['results'] = $product;
+        return response($out, 200);
+    }
+
+    //api to show product list
+    public function index()
+    {
+        $product = Product::select('name', 'price', 'description')->get();
+        $out['statuc'] = 'success';
+        $out['results'] = $product;
+        return response($out, 200);
+    }
+    
+    //api to add products
+    public function save(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string',
+            'price' => 'required|numeric',
+            'description' => 'required|string'
+        ]);
+        $product = Product::create($request->all());
+        $out['status'] = 'success';
+        return response($out, 200);
+    }
+}
